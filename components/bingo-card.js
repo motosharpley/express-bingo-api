f4157625312a035c3d2292c4a36968ef0871a15f
Bryan <motosharpley@gmail.com>
2019-04-11 16:20:30 -0500

    add bingo card component

---
+++ components/bingo-card.js
@@ -0,0 +1,39 @@
+let usedCardNums = new Array(76);
+let currentCardArray = new Array();
+
+function getNewNum() {
+  return Math.floor(Math.random() * 15);
+}
+
+function anotherCard() {
+  for (var i = 1; i < usedCardNums.length; i++) {
+    usedCardNums[i] = false;
+  };
+
+  newCard();
+  return false;
+}
+
+function newCard() {
+  for(var i=0 ; i<25 ; i++){
+      setSquare(i);
+    }
+}
+
+function setSquare(thisSquare){
+  // let colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4); // Row format
+  let colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4); // Coloumn format
+
+  let colBasis = colPlace[thisSquare] * 15;
+  let newNum = colBasis + getNewNum() + 1;
+
+
+  do{
+    newNum = colBasis + getNewNum() + 1;
+  }while(usedCardNums[newNum]);
+  
+  usedCardNums[newNum] = true;
+  return currentCardArray.push(newNum);  
+}
+newCard();
+console.log(currentCardArray);
