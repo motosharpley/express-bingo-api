461d201aacdf9a80f84475a568052b828cc3f00e
Bryan <motosharpley@gmail.com>
2019-05-08 14:20:13 -0500

    refactor to eneable binary on allBalls and interim

--- components/cardBinary.js
+++ components/cardBinary.js
@@ -3,28 +3,28 @@ const ballDraw = require('./ballDraw');
 
 const playerCard = bingoCard.currentCardArray;
 bingoCard.newCard();
-const interimDraw = ballDraw.allBallsDrawn;
-ballDraw.interimGameDraw();
+const interimDraw = ballDraw.interimGameDraw;
+const allBallsDrawn = ballDraw.allBallsDrawn;
+ballDraw.drawAllBalls();
 
 let cardBinary = new Array;
 
-function cardPatternBinary(currentCardArray, interimBallDraw) {
+function cardPatternBinary(currentCardArray, BallDraw) {
   
-  playerCard.forEach(square => {
-    if(interimDraw.includes(square)) {
+  currentCardArray.forEach(square => {
+    if(BallDraw.includes(square)) {
       cardBinary.push(1);
     } else {
       cardBinary.push(0);
     }
-  });
-  
+  });  
   return cardBinary;
 }
 
-cardPatternBinary(playerCard, interimDraw);
-console.log(cardBinary);
-setTimeout(() => {
-  console.log(interimDraw.sort());
-  console.log(playerCard);
-}, 1000);
-
+// Test logs
+// cardPatternBinary(playerCard, interimDraw);
+cardPatternBinary(playerCard, allBallsDrawn);
+// console.log('interimDraw: ' + cardBinary);
+console.log('allBallsDrawn: ' + cardBinary.reduce((accumulator, currentValue) => accumulator + currentValue ));
+// console.log(interimDraw.sort());
+// console.log(playerCard);
