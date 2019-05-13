07719b2fde16c6bcd0ce89e51a524e099252d7b6
Bryan <motosharpley@gmail.com>
2019-05-13 14:17:52 -0500

    refactor for module export

--- components/cardBinary.js
+++ components/cardBinary.js
@@ -1,30 +1,39 @@
-const bingoCard = require('./bingoCard');
-const ballDraw = require('./ballDraw');
+// ****** IMPORTS FOR TESTING ONLY *******
 
-const playerCard = bingoCard.currentCardArray;
-bingoCard.newCard();
-const interimDraw = ballDraw.interimGameDraw;
-const allBallsDrawn = ballDraw.allBallsDrawn;
-ballDraw.drawAllBalls();
+// const bingoCard = require('./bingoCard');
+// const ballDraw = require('./ballDraw');
 
-let cardBinary = new Array;
+// const playerCard = bingoCard.currentCardArray;
+// bingoCard.newCard();
+// const interimDraw = ballDraw.interimGameDraw;
+// const allBallsDrawn = ballDraw.allBallsDrawn;
+// ballDraw.drawAllBalls();
 
-function cardPatternBinary(currentCardArray, BallDraw) {
+// ******* END TESTING IMPORTS *******
+
+const cardBinary = {};
+
+cardBinary.currentCardBinary = new Array;
+
+cardBinary.cardPatternBinary = function(currentCardArray, BallDraw) {
   
   currentCardArray.forEach(square => {
     if(BallDraw.includes(square)) {
-      cardBinary.push(1);
+      cardBinary.currentCardBinary.push(1);
     } else {
-      cardBinary.push(0);
+      cardBinary.currentCardBinary.push(0);
     }
   });  
-  return cardBinary;
+  return cardBinary.currentCardBinary;
 }
+
+module.exports = cardBinary;
+
 
 // Test logs
 // cardPatternBinary(playerCard, interimDraw);
-cardPatternBinary(playerCard, allBallsDrawn);
+// cardPatternBinary(playerCard, allBallsDrawn);
 // console.log('interimDraw: ' + cardBinary);
-console.log('allBallsDrawn: ' + cardBinary.reduce((accumulator, currentValue) => accumulator + currentValue ));
+// console.log('allBallsDrawn: ' + cardBinary.reduce((accumulator, currentValue) => accumulator + currentValue ));
 // console.log(interimDraw.sort());
 // console.log(playerCard);
