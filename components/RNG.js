f32010536a96d87689807b8f0141684e4d350ffe
Bryan Sharpley <motosharpley@gmail.com>
2020-07-05 11:19:45 -0700

    fix to deliver whole numbers

--- components/RNG.js
+++ components/RNG.js
@@ -7,7 +7,8 @@ const RNG = {};
 RNG.getRandomNumber = function(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return (Math.random() * (max - min + 1)) + min;
+    num = (Math.random() * (max - min + 1)) + min;
+    return Math.round(num);
 }
 
 module.exports = RNG;
