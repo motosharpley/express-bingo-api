210a2712a89e8e5416dd67c99492fbf37f74687b
Bryan Sharpley <motosharpley@gmail.com>
2020-07-06 10:56:46 -0700

    stub out for pay table evaluation

---
+++ components/payEval.js
@@ -0,0 +1,13 @@
+// Symbol	            Type	5	    4	    3	    2
+// *1* FrankenCash 2	Wild	5000    100	    25	    10
+// *2* Frank	        Line	300	    50	    10	
+// *3* Bride	        Line	300	    50	    10	
+// *4* Doctor	        Line	300     50	    10	
+// *5* Igor	            Line	300	    50	    10	
+// *6* Chest	        Line	150	    30	    5	
+// *7* Tower	        Line	100	    20	    5	
+// *8* Brain	        Line	100	    15	    5	
+
+
+const lineEval = require('./lineEval');
+
