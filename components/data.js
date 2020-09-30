cf6278b7cf62c9fe4f6e352e76b99aed1ffa78dd
Bryan Sharpley <motosharpley@gmail.com>
2020-09-30 11:59:27 -0700

    extract data structures to central module

---
+++ components/data.js
@@ -0,0 +1,8 @@
+const data = {};
+
+data.spinResults = []; // Spin Results data structure [[0index array contains reel results], [line wins][scatter wins]]
+data.playResult = [];
+data.reelStops = [];
+data.reelResults = [];
+
+module.exports = data;
