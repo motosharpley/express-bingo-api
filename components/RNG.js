9fe42253af115b7cee5c3550f610b561751cff87
Bryan Sharpley <motosharpley@gmail.com>
2025-05-12 19:29:23 -0700

    add summary comment and range of random nums helper method

--- components/RNG.js
+++ components/RNG.js
@@ -1,4 +1,10 @@
-// ****** IMPORTS FOR TESTING ONLY *******
+/**
+ * Generates random numbers for the game.
+ *
+ * This module provides utility functions to generate random numbers
+ * within a specified range, ensuring fairness and unpredictability
+ * in the game mechanics.
+ */
 
 const RNG = {};
 
@@ -7,6 +13,15 @@ RNG.getRandomNumber = function (min, max) {
   max = Math.floor(max);
   num = Math.random() * (max - min + 1) + min;
   return Math.round(num);
+};
+
+// Get an array of random numbers for a within given range and count
+RNG.getRandomNumbers = function (min, max, count) {
+  const numbers = [];
+  for (let i = 0; i < count; i++) {
+    numbers.push(RNG.getRandomNumber(min, max));
+  }
+  return numbers;
 };
 
 module.exports = RNG;
